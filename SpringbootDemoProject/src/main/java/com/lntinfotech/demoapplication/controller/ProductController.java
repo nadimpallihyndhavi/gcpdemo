0bdb8c2fc446ba36006c0669c99ca8211e8d82e8
nadimpallihyndhavi <hyndhavi.abi@gmail.com>
2021-09-13 17:01:15 +0530

    swagger

--- SpringbootDemoProject/src/main/java/com/lntinfotech/demoapplication/controller/ProductController.java
+++ SpringbootDemoProject/src/main/java/com/lntinfotech/demoapplication/controller/ProductController.java
@@ -14,7 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lntinfotech.demoapplication.entity.Product;
 import com.lntinfotech.demoapplication.service.ProductService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value="ProductApplication")
 public class ProductController {
 
 	@Autowired
@@ -27,41 +31,42 @@ public class ProductController {
 	}
 
 	@PostMapping("/addProduct")
+	@ApiOperation(value = "save the Product", response = Product.class)
 	public Product addProduct(@RequestBody Product product) {
 		return service.saveProduct(product);
 
 	}
 	
 	@PostMapping("/addProducts")
+	@ApiOperation(value = "save the Products", response = Product.class)
 	public List<Product> addProduct(@RequestBody List<Product> products) {
 		return service.saveAllProduct(products);
 
 	}
 	
 	@GetMapping("/products")
+    @ApiOperation(value = "View list of Products")
 	public List<Product> findAllProducts(){
-		
 		return service.getAllProduct();
-		
 	}
 	
 	@GetMapping("/product/{id}")
+	@ApiOperation(value = "View Product by id")
 	public Product findProductById(@PathVariable int id) {
 		return service.getProductsById(id);
 	}
 	
 	@PutMapping("/update")
+	@ApiOperation(value = "update Product")
 	public Product updateProduct(@RequestBody Product product) {
 		return service.updateProduct(product);
 		
 	}
 	
 	@DeleteMapping("/delete/{id}")
+	@ApiOperation(value = "Delete an Product")
 	public String deleteProduct(@PathVariable int id) {
 		
 		return service.deleteProduct(id);
 	}
-	
-	
-	
 }
