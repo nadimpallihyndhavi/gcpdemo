3a83e7397198fd04516ca4674dcb0f6822dfb82a
nadimpallihyndhavi <hyndhavi.abi@gmail.com>
2021-09-13 18:21:26 +0530

    cloudappengine

--- SpringbootDemoProject/src/main/java/com/lntinfotech/demoapplication/entity/Product.java
+++ SpringbootDemoProject/src/main/java/com/lntinfotech/demoapplication/entity/Product.java
@@ -5,13 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "PRODUCT_TABLE")
 public class Product {
